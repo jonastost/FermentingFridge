19806acac5c0f44f40f4be73936b4fd54304568d
jonastost <jonastost@gmail.com>
2020-08-28 16:58:21 -0500

    Non-mubit version working

--- database_with_mubit.py
+++ database_with_mubit.py
@@ -8,7 +8,7 @@ import os
 
 tempup = 18
 tempdown = 16
-sleepingtime = 120
+sleepingtime = 60
 
 hostn = "localhost"
 databasen = "ferment_data"
--- database_without_mubit.py
+++ database_without_mubit.py
@@ -1,4 +1,4 @@
-import time
+import time as timer
 from datetime import datetime
 import mysql.connector as sql
 
@@ -18,7 +18,7 @@ cursor = mysql.cursor()
 current_str = ""
 
 while True:
-    delaystart = time.time()
+    delaystart = timer.time()
     cursor1 = mysql.cursor()
     cursor1.execute("SELECT brew_name_time, mode FROM All_Records WHERE done=false")
     currents = cursor1.fetchall()
@@ -29,54 +29,53 @@ while True:
         cursor4 = mysql.cursor()
         print(cur[0])
         try:
-            cursor3.execute("SELECT time FROM "+cur[0]+"_data ORDER BY row DESC LIMIT 1")
+            cursor3.execute("SELECT time FROM "+cur[0]+"_data LIMIT 1")
             three = cursor3.fetchall()
             print(three)
-            #cursor4.execute("SELECT time, row FROM "+cur+" ORDER BY row DESC LIMIT 1")
-            cursor4.execute("SELECT row FROM "+cur[0]+"_data")
+            cursor4.execute("SELECT row FROM "+cur[0]+"_data ORDER BY row DESC LIMIT 1")
+            #cursor4.execute("SELECT row FROM "+cur[0]+"_data")
             four = cursor4.fetchall()
         except (Exception):
-            four = None
+            four = ()
+            print("There was an exception")
+        print(four)
 
-        if (four is not None):
+        if (len(four)>0):
             for x in four:
                 latest = x[0]
                 row = latest+1
                 row = str(row)
                 print(row)
             for x in three:
-                time = time.time()-x[0]
+                time = timer.time()-float(x[0])
         else:
-            time = time.time()
+            time = timer.time()
             row = str(1)
         timestamp = datetime.now().strftime("%d-%m-%y %H:%M:%S")
-        if (cur[1] != "fridge" or cur[1] != "time"):
+        print(row)
+        if (cur[1] == "fridge" or cur[1] == "time"):
             current_temp = str(1000)
             average_temp = str(1000)
             
             time = str(time)
             timestamp = str(timestamp)
-            
-            cursor3.fetchall()
+            print(cur[0])
             cursor3.execute("INSERT INTO "+cur[0]+"_data (time, timestamp, current_temp, average_temp, row) VALUES ('"+time+"', '"+timestamp+"', "+current_temp+", "+average_temp+", "+row+")")
             mysql.commit()
         else:
             time = str(time)
             timestamp = str(timestamp)
-            cursor3.fetchall()
             cursor3.execute("INSERT INTO "+cur[0]+"_data (time, timestamp, row) VALUES ('"+time+"', '"+timestamp+"', "+row+")")
             mysql.commit()
         
-        if (float(time) > 1209600):
-            cursor3.fetchall()
-            cursor3.execute("UPDATE "+cur[0]+"_data SET done = true WHERE done = false")
+        if (float(time) > 1209600 and int(row) != 1):
+            cursor3.execute("UPDATE "+cur[0]+" SET done = true WHERE done = false")
             mysql.commit()
-            cursor3.fetchall()
-            cursor3.execute("UPDATE All_Records SET done = true WHERE brew_name_time = "+cur[0])
+            cursor3.execute("UPDATE All_Records SET done=true WHERE brew_name_time = '"+cur[0]+"'")
             mysql.commit()
-    delayend = time.time()
+    delayend = timer.time()
     delay = delayend - delaystart
     if (delay > 60):
         delay = 60
-    time.sleep(120-delay)
+    timer.sleep(120-delay)
     delay = 0
