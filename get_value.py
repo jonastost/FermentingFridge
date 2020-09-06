ed4dc19bfe9b28d2c47263d16fb6f4993437e052
jonastost <jonastost@gmail.com>
2020-09-05 23:09:22 -0500

    added serial parsing condition

--- get_value.py
+++ get_value.py
@@ -8,7 +8,7 @@ def getValue(previous):
     for ind in range(0, (20-len(test))):
         test = "a" + test
     test = findIterations(test)
-    while (test == 'none'):
+    while (test == 'none' and len(test) < 6):
         print('loop')
         test = str(service.read(25))
         test = test[2: len(test)-1]
@@ -18,6 +18,8 @@ def getValue(previous):
     connect = test
     if (len(test) == 5):
         connect = connect[0:3] + connect[4:5]
+    if (len(test) == 6):
+        connect = connect[0:3] + connect[5:6]
     connect = float(connect)
     return connect
 def findIterations(word):
